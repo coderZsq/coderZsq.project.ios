0b98c0f40a3bf7060aec8542462c9e086a06d86d
coderZsq <a13701777868@yahoo.com>
2018-10-22 17:08:55 +0800

    add

--- Re-Study-iOS/Network/Network/Project/Essence/Controller/SQSeeBigPictureViewController.m
+++ Re-Study-iOS/Network/Network/Project/Essence/Controller/SQSeeBigPictureViewController.m
@@ -8,6 +8,9 @@
 
 #import "SQSeeBigPictureViewController.h"
 #import <SDWebImage/UIImageView+WebCache.h>
+#import <SDImageCache.h>
+#import <SVProgressHUD.h>
+#import <Photos/Photos.h>
 
 @interface SQSeeBigPictureViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
@@ -21,8 +24,63 @@
 }
 
 - (IBAction)saveButtonClick:(UIButton *)sender {
-
+//    UIImage * image = [[SDImageCache sharedImageCache] imageFromCacheForKey:self.image0];
+//    UIImageWriteToSavedPhotosAlbum(_imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    if (status == PHAuthorizationStatusNotDetermined) {
+        [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+            if (status == PHAuthorizationStatusAuthorized) {
+                [self savePhoto];
+            }
+        }];
+    } else if (status == PHAuthorizationStatusAuthorized) {
+        [self savePhoto];
+    } else {
+        [SVProgressHUD showInfoWithStatus:@"进入设置界面->找到当前应用->打开允许访问相册"];
+    }
 }
+
+- (PHAssetCollection *)fetchAssetCollection:(NSString *)albumTitle {
+    PHFetchResult * result = [PHAssetCollection fetchAssetCollectionsWithType:(PHAssetCollectionTypeAlbum) subtype:(PHAssetCollectionSubtypeAlbumRegular) options:nil];
+    for (PHAssetCollection * assetCollection in result) {
+        if ([assetCollection.localizedTitle isEqualToString:albumTitle]) {
+            return assetCollection;
+        }
+    }
+    return nil;
+}
+
+- (void)savePhoto {
+    UIImage * image = self.imageView.image;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+        PHAssetCollection * assetCollection =[self fetchAssetCollection:@"Network"];
+        PHAssetCollectionChangeRequest * assetCollectionChangeRequest;
+        if (assetCollection) {
+            assetCollectionChangeRequest = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:assetCollection];
+        } else {
+            assetCollectionChangeRequest = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:@"Network"];
+            PHAssetChangeRequest * assetChangeRequest = [PHAssetChangeRequest creationRequestForAssetFromImage:image];
+            PHObjectPlaceholder * objectPlaceholder = [assetChangeRequest placeholderForCreatedAsset];
+            [assetCollectionChangeRequest addAssets:@[objectPlaceholder]];
+        }
+    } completionHandler:^(BOOL success, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (error) {
+                [SVProgressHUD showErrorWithStatus:@"保存失败"];
+            } else {
+                [SVProgressHUD showSuccessWithStatus:@"保存成功"];
+            }
+        });
+    }];
+}
+
+//- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+//    if (error) {
+//        [SVProgressHUD showErrorWithStatus:@"保存失败"];
+//    } else {
+//        [SVProgressHUD showSuccessWithStatus:@"保存成功"];
+//    }
+//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
