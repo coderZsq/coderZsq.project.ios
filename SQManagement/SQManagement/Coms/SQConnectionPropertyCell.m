91282cfd8783e0dbe8c26e8fe34eeb188e9d47c8
coderZsq <a13701777868@yahoo.com>
2019-09-25 14:19:26 +0800

    add

--- SQManagement/SQManagement/Coms/SQConnectionPropertyCell.m
+++ SQManagement/SQManagement/Coms/SQConnectionPropertyCell.m
@@ -10,4 +10,13 @@
 
 @implementation SQConnectionPropertyCell
 
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.iconImageView.layer.cornerRadius = 4;
+    self.iconImageView.layer.masksToBounds = YES;
+    self.iconImageView.layer.shadowColor = [UIColor lightGrayColor].CGColor;
+    self.iconImageView.layer.shadowOffset = CGSizeMake(2, 2);
+    self.iconImageView.layer.shadowOpacity = 0.8;
+}
+
 @end
