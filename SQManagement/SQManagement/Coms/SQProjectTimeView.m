2cf9bdfa7e10b454e24bb4db2df6d1e30ad9e83d
coderZsq <a13701777868@yahoo.com>
2019-09-29 11:21:44 +0800

    add

---
+++ SQManagement/SQManagement/Coms/SQProjectTimeView.m
@@ -0,0 +1,21 @@
+//
+//  SQProjectTimeView.m
+//  SQManagement
+//
+//  Created by 朱双泉 on 2019/9/29.
+//  Copyright © 2019 Castie!. All rights reserved.
+//
+
+#import "SQProjectTimeView.h"
+
+@implementation SQProjectTimeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
